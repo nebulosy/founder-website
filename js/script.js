749352204d2cb4a7fee7ae535b52cde4cddf5bc4
Samuel Tessaro <samueldesouzatessaro@gmail.com>
2022-05-24 12:25:03 -0300

    Synthesized some functions.
    Some functions have been improved for code simplification. Only one function was created for displaying or not and only one for animating the image to the side.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function showDiv(idDiv){
-    let div = document.querySelector("#"+ idDiv);
-    div.style.display = "block";
-}
-
-function hideDiv(idDiv){
-    let div = document.querySelector("#"+ idDiv);
-    div.style.display = "none";
+function showOrHideDiv(idDiv,show){
+    if(show == true){
+        let div = document.querySelector("#"+ idDiv);
+        div.style.display = "block";
+    }else{
+        let div = document.querySelector("#"+ idDiv);
+        div.style.display = "none";
+    }
 }
 
 function divStatus(idDiv){
@@ -14,30 +14,21 @@ function divStatus(idDiv){
     return divSituation;
 }
 
-function slideImage(idDiv){
+function slideImage(idDiv,slideSize){
     let div = document.querySelector("#"+ idDiv);
-    div.style.marginLeft = "-12%";
-
-}
-
-function slideImageToOriginal(idDiv){
-    let div = document.querySelector("#"+ idDiv);
-    div.style.marginLeft = "0%";
-
+    div.style.marginLeft = (slideSize+"%");
 }
 
 function aboutText(){
     let divName = ("about-text");
     let divSituation = divStatus(divName);
 
-    let imagePrincipalDivName = ("principal-image");
-
     if (divSituation != "block"){
-        showDiv(divName);
-        slideImage("principal-image");
+        showOrHideDiv(divName,true);
+        slideImage("principal-image",-12);
     }else{
-        hideDiv(divName)
-        slideImageToOriginal("principal-image");
+        showOrHideDiv(divName,false);
+        slideImage("principal-image",0);
     }
 }
 
